1bb29cdd5ddd8d12d0f20ceb813a0365187c1b98
jeffreyd3308 <jeffreyd3308@nycstudents.net>
2025-01-13 16:37:02 +0000

    comet

---
+++ U5T9/Main4.java
@@ -0,0 +1,17 @@
+public class Main4 {
+    public static void main(String[] args) {
+        Box b1 = new Box(10);
+        Shape shape = new Shape();
+        b1.addToShape(shape);
+        Box shapeBox = shape.getBox();
+        System.out.println(b1 == shapeBox);
+        System.out.println(shapeBox.boxInfo());
+        System.out.println();
+        Box b2 = new Box(20, 30, 40);
+        b2.addToShape(shape);
+        shapeBox = shape.getBox();
+        System.out.println(b2 == shapeBox);
+        System.out.println(shapeBox.boxInfo());
+            
+    }
+}
